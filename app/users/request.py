578e252388e0be9d2e304b2d769c5be01bf860ba
Lorenzo Murarotto <lnzmrr@gmail.com>
2022-12-06 20:01:21 +0000

    improve user validation

--- app/users/request.py
+++ app/users/request.py
@@ -2,22 +2,25 @@ from typing import Optional
 
 from pydantic import BaseModel, EmailStr, constr, root_validator
 
+NAME_MAX_LEN = 32
+PASSWORD_MAX_LEN = 256
+
 
 class CreateUserRequest(BaseModel):
     email: EmailStr
-    name: Optional[str]
-    password: constr(min_length=1)
+    name: Optional[constr(min_length=1, max_length=NAME_MAX_LEN)]
+    password: constr(min_length=1, max_length=PASSWORD_MAX_LEN)
 
 
 class UpdateUserRequest(BaseModel):
     email: Optional[EmailStr]
-    name: Optional[str]
+    name: Optional[constr(min_length=1, max_length=NAME_MAX_LEN)]
 
 
 class LoginUserRequest(BaseModel):
     email: Optional[EmailStr]
-    name: Optional[str]
-    password: str
+    name: Optional[constr(min_length=1, max_length=NAME_MAX_LEN)]
+    password: constr(min_length=1, max_length=PASSWORD_MAX_LEN)
 
     @root_validator()
     @classmethod
