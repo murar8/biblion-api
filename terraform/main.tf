a36ea4ebba8bd9147542646a4ed2cb5a8a7c33de
Lorenzo Murarotto <lnzmrr@gmail.com>
2022-12-11 14:36:21 +0100

    improve secret management (#4)

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,9 +15,13 @@ locals {
     "cloudresourcemanager.googleapis.com",
     "artifactregistry.googleapis.com",
     "iam.googleapis.com",
-    "run.googleapis.com"
+    "run.googleapis.com",
+    "secretmanager.googleapis.com"
   ]
 }
+
+# IMPROVEMENT: add some dead time after enabling the required APIs to allow for
+# the changes to propagate.
 
 resource "google_project_service" "project_services" {
   for_each                   = toset(local.project_services)
